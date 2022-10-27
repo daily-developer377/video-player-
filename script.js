f5f8f08d08faa710ce33bf23767fed2f6472a778
Nandhu sunil <nandhusunil377@gmail.com>
2022-10-28 00:16:13 +0530

    commit

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let playBtn = document.getElementById("play-btn");
 let video = document.querySelector(".video");
+let progressBar = document.querySelector(".progress-bar");
+let progressRange = document.querySelector(".progress-range");
 
 let isVideoPlaying = false;
 
@@ -17,6 +19,26 @@ function playOrPauseVideo() {
     }
 }
 
+// update progress bar
+
+function updateProgressbar(Event) {
+    let currentTime = Event.target.currentTime;
+    let duration = Event.target.duration;
+    progressBar.style.cssText = `width:${(currentTime / duration ) * 100}%` ;
+     
+}
+
+//update seek bar//
+
+function updateSeekBar(event) {
+    let currentPoint = event.offsetX;
+    let progressbarWidth = this.clientWidth;
+    let currentRange = (currentPoint / progressbarWidth) * video.duration;
+    video.currentTime = currentRange;
+}
+
 //Add Event Listners//
-playBtn.addEventListener('click', playOrPauseVideo);
-video.addEventListener('click',playOrPauseVideo);
+playBtn.addEventListener("click", playOrPauseVideo);
+video.addEventListener("click",playOrPauseVideo);
+video.addEventListener("timeupdate", updateProgressbar);
+progressRange.addEventListener("click", updateSeekBar);
